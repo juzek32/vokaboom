59d38c50c2c931b120bc7cec1061ba8e41e47b7a
Jozef Šifra <70467972+juzek32@users.noreply.github.com>
2024-09-07 13:08:29 +0200

    switched to a different approach, using a list of most common german words with translation and plurals and articles which then gets processed into a list

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,18 @@
-from PyMultiDictionary import MultiDictionary
-dictionary = MultiDictionary()
+from ui import *
 
-#imports a list of 3000 most common english words as a list
+
+#imports a list of german words and their translations
 words_file = open("./../data/words.txt", "r")
 words_str = words_file.read()
 words = words_str.split("\n")
+for i, item in enumerate(words):
+    words[i] = item.split(" ")
+    words[i][2] = f"{words[i][2]} {words[i][3]}"
+    if len(words[i]) < 6:
+        words[i][3] = "-"
+        words[i] = words[i][:-1]
+    else:
+        words[i][3] = f"{words[i][4]} {words[i][5]}"
+        words[i] = words[i][:-2]
 words_file.close()
+
