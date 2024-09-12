8f0f6fbc2ac404e638f90ac9bf9ec873dc7aded7
Jozef Šifra <70467972+juzek32@users.noreply.github.com>
2024-09-12 20:01:53 +0200

    different ui approach, keeping things simple

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,9 @@
-from ui import *
+import tkinter as tk
+from tkinter import *
 
 
 #imports a list of german words and their translations
-words_file = open("./../data/words.txt", "r")
+words_file = open("./data/words.txt", "r")
 words_str = words_file.read()
 words = words_str.split("\n")
 for i, item in enumerate(words):
@@ -16,3 +17,22 @@ for i, item in enumerate(words):
         words[i] = words[i][:-2]
 words_file.close()
 
+root = tk.Tk()
+root.title("Vokaboom!")
+root.geometry("1000x600")
+
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=2)
+
+root.rowconfigure(0, weight=3)
+root.rowconfigure(1, weight=5)
+root.rowconfigure(2, weight=2)
+root.rowconfigure(3, weight=3)
+root.rowconfigure(4, weight=2)
+
+guess = StringVar()
+
+guess_entry = tk.Entry(root, textvariable=guess)
+guess_entry.grid(column=1, row=1)
+
+root.mainloop()
